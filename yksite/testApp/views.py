b749691e2b739a2bdbe4427fe932dc65927e8b91
yksung <maria.yk.sung@gmail.com>
2015-04-23 01:05:14 +0900

    검색결과 도

--- yksite/testApp/views.py
+++ yksite/testApp/views.py
@@ -13,12 +13,14 @@ def search(request):
     # timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d %H:%M:%S')
     
     # Send the GET request
-    url = 'http://52.68.159.245:7800/search/search_json.jsp'      
+    url = 'http://52.68.159.245:7800/search/search_json.jsp?collection=sample_terms'      
     req = urllib2.Request(url)
     
     # Read the response
     resp = urllib2.urlopen(req).read()
     
-    print resp['SearchQueryResult']['Collection']['DocumentSet']
+    result = json.loads(resp)
+    resultObj = result['SearchQueryResult']['Collection'][0]['DocumentSet']['Document']
     
-    return render_to_response('main.html', {"resultObj": json.loads(resp)})
+    return render_to_response('main.html', {"resultObj": resultObj
+                                            , "json" : json.dumps(result)})
