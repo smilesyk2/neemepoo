8f01eb747ed5e18fdbc1be4e267e503f98d4c259
Youkyung Sung <maria.yk.sung@gmail.com>
2015-04-23 18:06:06 +0900

    검색어, 컬렉션 선택 기능 추가

--- yksite/testApp/views.py
+++ yksite/testApp/views.py
@@ -1,26 +1,42 @@
 from django.shortcuts import render, render_to_response
+from django.views.decorators.csrf import csrf_exempt
 import urllib2
+import urllib
 import time
 import datetime
 import json
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 # Create your views here.
 def home(request):
     return render_to_response('index.html')
 
+@csrf_exempt
 def search(request):
     # Set the request authentication headers
     # timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d %H:%M:%S')
-    
     # Send the GET request
-    url = 'http://52.68.159.245:7800/search/search_json.jsp?collection=sample_terms'      
-    req = urllib2.Request(url)
+    url = 'http://52.68.159.245:7800/search/search_json.jsp'
     
+    data = {}
+    if request.method == 'POST':
+        for key in request.POST:
+            data.update({key: request.POST[key]})
+
+        print '@@@ data :::', json.dumps(data)
+        
+    print url
+    req = urllib2.Request(url, json.dumps(data)) if data!='' else urllib2.Request(url)
     # Read the response
-    resp = urllib2.urlopen(req).read()
+    response = urllib2.urlopen(req).read()
     
-    result = json.loads(resp)
+    result = json.loads(response)
     resultObj = result['SearchQueryResult']['Collection'][0]['DocumentSet']['Document']
     
+    print resultObj
+    
     return render_to_response('main.html', {"resultObj": resultObj
+                                            , "data" : json.dumps(data)
                                             , "json" : json.dumps(result)})
