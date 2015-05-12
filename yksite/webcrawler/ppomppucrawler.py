09736fcd22e83cfe02c924376a9e952c14e30a48
Youkyung Sung <maria.yk.sung@gmail.com>
2015-05-12 11:50:02 +0900

    컨텐츠 수집

--- yksite/webcrawler/ppomppucrawler.py
+++ yksite/webcrawler/ppomppucrawler.py
@@ -12,11 +12,23 @@ soup = BeautifulSoup(response, parse_only=SoupStrainer('a', href=True))
 cnt = 0
 for link in soup.find_all('a'):
     cnt = cnt+1
-    print '<%d>' % cnt
+    print '<%d> %s' % (cnt, link)
     
-    if link.has('font') and link.font['class']=='list_title':
+    if link.font and 'list_title' in link.font['class']:
         # 제목 추출
         print 'title ::: %s' % link.text
+        # 내용 링크 추출
+        print 'href ::: %s' % link['href']
+        # 내용 읽기
+        status, contResponse = http.request('http://www.ppomppu.co.kr/zboard/'+link['href'])
+        cont = BeautifulSoup(contResponse, parse_only=SoupStrainer('table'))
         
-    # 내용 링크 추출
-    print 'href ::: %s' % link['href']
+        for table in cont.find_all('table'):
+            print table['class'] if 'class' in table else '((no class tags in table node))'
+#             if 'class' in table and 'info_bg' in table['class']:
+#                 print 'info ::: %s' % table.text
+#             if 'class' in table and 'pic_bg' in table['class']:
+#                 print 'cont ::: %s'  % table.text
+        
+        
+        
